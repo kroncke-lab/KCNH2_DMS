045609231f8537fc982e7bec4b2a6c60448ae8f3
Brett Kroncke <39835074+kroncke-lab@users.noreply.github.com>
2021-05-24 16:41:28 -0500

    Intermediate step

--- c_bcvarfiles.py
+++ c_bcvarfiles.py
@@ -1,5 +1,6 @@
 import pandas as pd
 #  TODO: I need to keep in mind that a MAJOR feature I want is to be able to add new sequencing results directly to the already processed reads.
+#  TODO: I need to compare each line with the "wt" discovered in the first part of the script.
 
 
 #  compress variant files and add counts, remove n=1 (maybe n=2, too?)
@@ -12,6 +13,8 @@ def reduce_fastq(chunk, file):
         df_filt_nonwt.to_csv(f, header=False)
 
 # Count unique barcode-variant pairs, discard n=1 observations (maybe also remove n=2, too?), save file 'to_feather'?
+folder = "E:\\Tile1-Library2-LV310\\barcode-key\\20201006-5406-LVkeyRepeat\\"  # input file
+
 i = 0
 file = folder + "data/" + str(i) + "th_fastq.tmp"
 reduce_fastq(file)
@@ -21,3 +24,21 @@ df_chunks = pd.read_csv(file, names=["bc", "wt", "seq"], chunksize=2000000)
     frames = [reduce_fastq(chunk) for chunk in df_chunks]
 pd.df_filt_nonwt.to_feather
 
+
+from functools import reduce
+
+def get_counts(chunk):
+    voters_street = chunk[
+        "Residential Address Street Name "]
+    return voters_street.value_counts()
+
+def add(previous_result, new_result):
+    return previous_result.add(new_result, fill_value=0)
+
+# MapReduce structure:
+chunks = pandas.read_csv("voters.csv", chunksize=1000)
+processed_chunks = map(get_counts, chunks)
+result = reduce(add, processed_chunks)
+
+result.sort_values(ascending=False, inplace=True)
+print(result)
--- main.py
+++ main.py
@@ -16,7 +16,12 @@ i = 0
 for files in files_rev:
     wt = models.consensus.findconsensus(files)
     models.consensus.write_bcvar(files, files_barcode[i], folder + "data/" + str(i) + "th_fastq.tmp.wt",
-                                 "data\\var" + str(i), wt)
+                                 folder + "data/" + str(i) + "var", wt)
     with open(folder + "data/" + str(i) + "th_fastq.wt", 'w') as out_handle:
         out_handle.write(wt)
     i += 1
+
+#  read "wt" and compare with plasmid sequence:
+#  check to see if barcode is included in the read? Should this already be removed in the first step?
+# I could check for the sequence flanking either side of the barcode and split based on that match?
+# I think the step which determines the "wt" sequence should also establish the frame of the sequenced segment.
