d4f5bd4f6b54a5ec436d07bd195a65a080657f9f
Brett Kroncke <39835074+kroncke-lab@users.noreply.github.com>
2021-06-02 16:21:28 -0500

    Merge branch 'master' into gdmn
    # Conflicts:
    #	main.py

--- models/c_bcvarfiles.py
+++ models/c_bcvarfiles.py
@@ -1,26 +1,44 @@
 import pandas as pd
+#  TODO: I need to keep in mind that a MAJOR feature I want is to be able to add new sequencing results directly to the already processed reads.
+#  TODO: I need to compare each line with the "wt" discovered in the first part of the script.
+
+
+#  compress variant files and add counts, remove n=1 (maybe n=2, too?)
+#  probably just write to csv?
+def reduce_fastq(chunk, file):
+    df_min = chunk.groupby(chunk.columns.tolist(), as_index=False).size()
+    df_filt = df_min[df_min['size'] > 1]
+    df_filt_nonwt = df_filt[df_filt['wt'] != 'wt']
+    with open(file, 'a') as f:
+        df_filt_nonwt.to_csv(f, header=False)
+
+# Count unique barcode-variant pairs, discard n=1 observations (maybe also remove n=2, too?), save file 'to_feather'?
+folder = "E:\\Tile1-Library2-LV310\\barcode-key\\20201006-5406-LVkeyRepeat\\"  # input file
+
+i = 0
+file = folder + "data/" + str(i) + "th_fastq.tmp"
+reduce_fastq(file)
+
+df_chunks = pd.read_csv(file, names=["bc", "wt", "seq"], chunksize=2000000)
+
+    frames = [reduce_fastq(chunk) for chunk in df_chunks]
+pd.df_filt_nonwt.to_feather
+
+
 from functools import reduce
 
-# TODO: I need to keep in mind that a MAJOR feature I want is to be able to add new sequencing results directly
-#  to the already processed reads.
-
-
 def get_counts(chunk):
-    uniq_bc_pair = chunk[["bc", "wt", "seq"]]
-    return uniq_bc_pair.value_counts()
-
+    voters_street = chunk[
+        "Residential Address Street Name "]
+    return voters_street.value_counts()
 
 def add(previous_result, new_result):
     return previous_result.add(new_result, fill_value=0)
 
+# MapReduce structure:
+chunks = pandas.read_csv("voters.csv", chunksize=1000)
+processed_chunks = map(get_counts, chunks)
+result = reduce(add, processed_chunks)
 
-def find_and_reduce_bcvars(file):
-    chunks = pd.read_csv(file, names=["bc", "wt", "seq"], chunksize=500000)
-    processed_chunks = map(get_counts, chunks)
-    #  this next part takes ~ 15 min.
-    result = reduce(add, processed_chunks)
-    # la siquiente linea deja de los barcode-variadades llaves visto solo uno o dos veces.
-    result = result[result > 5]
-    return result
-
-
+result.sort_values(ascending=False, inplace=True)
+print(result)
--- models/labelvariant.py
+++ models/labelvariant.py
@@ -1,43 +1,18 @@
-import numpy as np
+import Bio.Seq
+from Bio import SeqIO
 
+# I want to match the full sequence of KCNH2 with the read and pull out all the variants and calculate HGSVg/p
+folder = 'E:\\Tile1-Library2-LV310\\barcode-key\\20201006-5406-LVkeyRepeat\\'
 
-# here I should compare the plasmid seq. with the read 'wt' if DNA in 'wt' doesn't match the gene
-def check_outside_cDNA(wt_gene, plasmid, test_seq):
-    adj_start = False
-    from_beginning = True
-    wt_gene = wt_gene.upper()
-    plasmid = plasmid.upper()
-    test_seq = test_seq.upper()
-    if wt_gene.seq.find(test_seq) < 0:
-        tmp = plasmid.seq.split(test_seq)
-        adj_start = True
-        #  the 6,000 below comes from the place of KCNH2 in the full plasmid: there are 3,118 nucleotides before KCNH2
-        #  starts, so 6,000 is safe since here I only need to tell if its the sequencing goes beyond the beginning or
-        #  follows after the end
-        if (len(tmp[0]._data) < 6000):
-            from_beginning = True
-            seq_use = wt_gene.seq.split(tmp[1]._data[0:200])[0]
-            len_use = len(test_seq.split(seq_use)[0]._data)
-            return adj_start, from_beginning, len_use
-        elif (len(tmp[0]._data) > 6000):
-            from_beginning = False
-            seq_use = wt_gene.seq.split(tmp[0]._data[-200:])[1]
-            len_use = len(test_seq.split(seq_use)[1]._data)
-            return adj_start, from_beginning, len_use
-        else:
-            raise ValueError(
-                "Sequence given not found in plasmid provided."
-            )
-    else:
-        return adj_start, from_beginning, 0
+test = SeqIO.parse(folder+"data\\plasmid.fasta", 'fasta')
+plasmid = next(test)
+wt_gene = next(test)
+tmp_wt = wt_gene.seq._data
+tmp_wt = tmp_wt.upper()
+wt_prot = wt_gene.translate()
 
-def find_frame(read_seq_rev, wt_prot):
-    n = np.arange(3)
-    for j in np.nditer(n):
-        read_prot = read_seq_rev[j:].translate()
-        if (wt_prot.seq.find(read_prot) > 0):
-            return wt_prot.seq.find(read_prot), j.max()
-        else:
-            continue
-    print("Frame for translation not found")
+read = open(folder + "data\\2th_fastq.wt", 'r').readline()
+
+read_seq = Bio.Seq.Seq(read)
+read_seq_rev = read_seq.reverse_complement()
 
